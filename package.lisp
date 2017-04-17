3f9cb610cf6633f088d2d7cb9217253cf31b32e0
Elijah Malaby <qwe12345678910@gmail.com>
2017-04-17 08:36:15 -0400

    Corrected exports to reflect defvar instead of defconstant.

--- package.lisp
+++ package.lisp
@@ -6,9 +6,11 @@
 
 (defpackage #:cl-pwn/util
   (:use #:cl #:cl-pwn #:generators)
-  (:export #:+ascii-lowercase+
-           #:+ascii-uppercase+
-           #:+ascii-letters+
-           #:+digits+
-           #:+hex-digits+
-           #:+punctuation+))
+  (:export #:*whitespace*
+           #:*ascii-lowercase*
+           #:*ascii-uppercase*
+           #:*ascii-letters*
+           #:*digits*
+           #:*hex-digits*
+           #:*octal-digits*
+           #:*punctuation*))
