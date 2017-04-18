76572dbe1c9adb14d81a892b727c5606006e43cb
Kevin Orr <kevinorr54@gmail.com>
2017-04-18 03:50:01 -0400

    Added very slow non-generator de-bruijn function

--- util/cyclic.lisp
+++ util/cyclic.lisp
@@ -10,7 +10,7 @@
 (defvar *punctuation* "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~")
 (defvar *printable* (concatenate 'string *digits* *ascii-letters* *punctuation* *whitespace*))
 
-(defun de-bruijn (&key (alphabet *ascii-lowercase*) (n 4))
+(defun de-bruijn-gen (&key (alphabet *ascii-lowercase*) (n 4))
   "Generator for a sequence of unique substrings of length n"
   ;; Taken from https://en.wikipedia.org/wiki/De_Bruijn_sequence and pwntools source
   ;; TODO figure out why this algorithm works
@@ -40,3 +40,60 @@
                       (setf (aref a s) j)
                       (yielding (db (1+ s) s))))))))
       (db 1 1))))
+
+(defun de-bruijn (&key (alphabet *ascii-lowercase*) (n 4))
+  (let* ((k (length alphabet))
+         (a (make-array (* k n)
+                        :element-type 'fixnum
+                        :initial-element 0))
+         (seq nil))
+
+    (labels ((db (s p)
+               (cond
+                 ((> s n)
+                  (when (= 0 (mod n p)))
+                  (setf seq (nconc
+                             seq
+                             (coerce (subseq a 1 (1+ p)) 'list))))
+                 (t
+                  (setf (aref a s) (aref a (- s p)))
+                  (db (1+ s) p)
+                  (loop :for j :from (1+ (aref a (- s p))) :below k :do
+                    (setf (aref a s) j)
+                    (db (1+ s) s))))))
+      (db 1 1)
+      (map 'list (lambda (i) (aref alphabet i)) seq))))
+
+(defun cyclic (&key (length nil) (alphabet *ascii-lowercase*) (n 4))
+  "cyclic &key (length nil) (alphabet *ascii-lowercase*) (n 4) => string
+
+   A simple wrapper over #'de-bruijn. This function returns at most
+   `length' elements.
+
+   Arguments:
+       length: The desired length of the list or None if the entire sequence is desired.
+       alphabet: List or string to generate the sequence over.
+       n(int): The length of subsequences that should be unique.
+
+   Value:
+       string: represents the de Bruijn sequence of maximum length `length',
+               composed of alphabet `alhpabet', with distinct subsequences of length `n'
+
+    Example:
+        (cyclic :alphabet \"ABC\" :n 3)
+        => \"AAABAACABBABCACBACCBBBCBCCC\"
+        (cyclic :length 20)
+        \"aaaabaaacaaadaaaeaaa\"
+        (let ((alphabet (loop :for i :from 0 :below 30 :collect i))
+              (n 3))
+          (values
+           (expt (length alphabet) n)
+           (length (cyclic :alphabet alphabet :n n))))
+        =>  27000, 27000
+    "
+
+  (assert (or (null length) (>= (expt (length alphabet) n) length)))
+
+  (coerce (loop :repeat (or length (expt (length alphabet) n))
+                :for i in (de-bruijn :alphabet alphabet :n n) :collect i)
+          'string))
