02e5be79ae2fd291633dee2968c2afa922301c35
Kevin Orr <kevinorr54@gmail.com>
2017-05-05 13:47:02 -0400

    Learning more from PCL chapter 24

--- util/packing.lisp
+++ util/packing.lisp
@@ -1,28 +1,26 @@
 (in-package :cl-pwn/util)
 
 ;; Begin awesome PCL chapter 24 stuff
-(defun as-keyword (sym) (intern (string sym) :keyword))
+(defgeneric read-value (type stream &key &allow-other-keys)
+  (:documentation "Read a value of the given type from the stream."))
+(defmethod read-value ((type symbol) stream &key)
+  (let ((object (make-instance type)))
+    (read-object object stream)
+    object))
 
-(defun binary-class-slot->defclass-slot (spec)
-  (let ((name (first spec)))
-    `(,name :initarg ,(as-keyword name) :accessor ,name)))
+(defgeneric write-value (type stream value &key &allow-other-keys)
+  (:documentation "Write a value as the given type to the stream."))
+(defmethod write-value ((type symbol) stream value &key)
+  (assert (typep value type))
+  (write-object value stream))
 
-(defmacro with-gensyms ((&rest names) &body body)
-  `(let ,(loop for n in names collect `(,n (gensym)))
-     ,@body))
+(defgeneric read-object (object stream)
+  (:method-combination progn :most-specific-last)
+  (:documentation "Fill in the slots of object from stream."))
 
-(defmacro define-binary-class (name &body body)
-  (let ((slots (first body)))
-    (with-gensyms (typevar objectvar streamvar)
-      `(progn
-         (defclass ,name ()
-           ,(mapcar #'binary-class-slot->defclass-slot slots))
-
-         (defmethod read-value ((,typevar (eql ',name)) ,streamvar &key)
-           (let ((,objectvar (make-instance ',name)))
-             (with-slots ,(mapcar #'first slots) ,objectvar
-               ,@(mapcar #'(lambda (x) (slot->read-value x streamvar)) slots))
-             ,objectvar))))))
+(defgeneric write-object (object stream)
+  (:method-combination progn :most-specific-last)
+  (:documentation "Write out the slots of object to the stream."))
 
 (defun mklist (x) (if (listp x) x (list x)))
 
@@ -33,12 +31,63 @@
   (destructuring-bind (name (type &rest args)) (normalize-slot-spec spec)
     `(setf ,name (read-value ',type ,stream ,@args))))
 
+(defun binary-class-slot->write-value (spec stream)
+  (destructuring-bind (name (type &rest args)) (normalize-slot-spec spec)
+    `(write-value ',type ,stream ,name ,@args)))
+
+
+(defmacro with-gensyms ((&rest names) &body body)
+  `(let ,(loop :for n :in names :collect `(,n (gensym)))
+     ,@body))
+
+(defun as-keyword (sym) (intern (string sym) :keyword))
+
+(defun binary-class-slot->defclass-slot (spec)
+  (let ((name (first spec)))
+    `(,name :initarg ,(as-keyword name) :accessor ,name)))
+
+(defmacro define-binary-class (name (&rest superclasses) &body body)
+  (let ((slots (first body)))
+    (with-gensyms (objectvar streamvar)
+      `(progn
+         (defclass ,name ,superclasses
+           ,(mapcar #'binary-class-slot->defclass-slot slots))
+
+         (eval-when (:compile-toplevel :load-toplevel :execute)
+           (setf (get ',name 'slots) ',(mapcar #'first slots))
+           (setf (get ',name 'superclasses) ',superclasses))
+
+         (defmethod read-object progn ((,objectvar ,name) ,streamvar)
+           (with-slots ,(new-class-all-slots slots superclasses) ,objectvar
+             ,@(mapcar #'(lambda (x) (binary-class-slot->read-value x streamvar)) slots)))
+
+         (defmethod write-object progn ((,objectvar ,name) ,streamvar)
+           (with-slots ,(new-class-all-slots slots superclasses) ,objectvar
+             ,@(mapcar #'(lambda (x) (binary-class-slot->write-value x streamvar)) slots)))))))
+
+(defun direct-slots (name)
+  (copy-list (get name 'slots)))
+
+(defun inherited-slots (name)
+  (loop :for super :in (get name 'superclasses)
+        :nconc (direct-slots super)
+        :nconc (inherited-slots super)))
+
+(defun all-slots (name)
+  (nconc (direct-slots name) (inherited-slots name)))
+
+(defun new-class-all-slots (slots superclasses)
+  (nconc (mapcan #'all-slots superclasses) (mapcar #'first slots)))
+
+
 ;; Testing above functions/macros
-#+()
-(define-binary-class id3-tag
+(define-binary-class id3-tag ()
   ((identifier      (iso-8859-1-string :length 3))
    (major-version   u1)
    (revision        u1)
    (flags           u1)
    (size            id3-tag-size)
    (frames          (id3-frames :tag-size size))))
+
+(define-binary-class id3-tag-extra (id3-tag)
+  ((extra-stuff     (id3-frames :tag-size size))))
